2b75b4af4cedc7d8d4c205e46d5c70e334582468
RANZN <aryaprakashranjan@gmail.com>
2021-09-29 10:02:03 +0530

    initial

--- app/src/main/java/com/example/pizza/Fragments/Home_Fragment.java
+++ app/src/main/java/com/example/pizza/Fragments/Home_Fragment.java
@@ -1,16 +1,15 @@
 package com.example.pizza.Fragments;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
@@ -24,7 +23,7 @@ import java.util.ArrayList;
 public class Home_Fragment extends Fragment {
     private ImageSlider imageSlider;
     private ArrayList<SlideModel> imageList=new ArrayList<>();
-    private ArrayList<Image_model>   imageButtons=new ArrayList<>();
+    private ArrayList<Image_model>  imageButtons=new ArrayList<>();
     private RecyclerView recyclerView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,38 +35,27 @@ public class Home_Fragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        imageList.add(new SlideModel(R.drawable.pizza1, ScaleTypes.FIT));
-        imageList.add(new SlideModel(R.drawable.pizza1, ScaleTypes.FIT));
-        imageList.add(new SlideModel(R.drawable.pizza1, ScaleTypes.FIT));
-        imageList.add(new SlideModel(R.drawable.pizza1, ScaleTypes.FIT));
-        initviews(view);
+        imageList.add(new SlideModel(R.drawable.a, ScaleTypes.FIT));
+        imageList.add(new SlideModel(R.drawable.b, ScaleTypes.FIT));
+        imageList.add(new SlideModel(R.drawable.c, ScaleTypes.FIT));
+        initViews(view);
+        buildData();
         imageSlider.setImageList(imageList);
+        setRecyclerView();
 
     }
-    private void buildStudentList() {
-        for (int i=0;i<20;i++)
-        {
-            if(i%4==0)
-            {
-                Image_model model=new Image_model(R.drawable.pizza1);
-                imageButtons.add(model);
-            }
-            else if(i%4==1)
-            {
-                Image_model model=new Image_model(R.drawable.pizza1);
-                imageButtons.add(model);
-            }
-            else if(i%4==2)
-            {
-                Image_model model=new Image_model(R.drawable.pizza1);
-                imageButtons.add(model);
-            }
-            else if(i%4==3)
-            {
-                Image_model model=new Image_model(R.drawable.pizza1);
-                imageButtons.add(model);
-            }
-        }
+
+    private void buildData() {
+                imageButtons.add(new Image_model(R.drawable.d));
+                imageButtons.add(new Image_model(R.drawable.e));
+                imageButtons.add(new Image_model(R.drawable.f));
+                imageButtons.add(new Image_model(R.drawable.g));
+                imageButtons.add(new Image_model(R.drawable.h));
+                imageButtons.add(new Image_model(R.drawable.i));
+                imageButtons.add(new Image_model(R.drawable.j));
+                imageButtons.add(new Image_model(R.drawable.k));
+                imageButtons.add(new Image_model(R.drawable.l));
+                imageButtons.add(new Image_model(R.drawable.m));
     }
 
     private void setRecyclerView() {
@@ -77,7 +65,7 @@ public class Home_Fragment extends Fragment {
         recyclerView.setAdapter(adapter);
     }
 
-    private void initviews(View view) {
+    private void initViews(View view) {
         imageSlider=view.findViewById(R.id.image_slider);
         recyclerView=view.findViewById(R.id.recycler1);
     }
